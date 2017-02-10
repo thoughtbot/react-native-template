b6b53b69f43513858b96621d14d6d86740dd0741
Adam & Mike <adam+mwenger@thoughtbot.com>
2017-02-10 14:01:16 -0500

    Extract navigator definitions into routes.js

--- TemplateProject/app/components/app.js
+++ TemplateProject/app/components/app.js
@@ -1,38 +1,7 @@
 import React from 'react';
 import { StatusBar, StyleSheet, View } from 'react-native';
-import { StackNavigator, TabNavigator } from 'react-navigation';
-import { Counter, Welcome } from '../screens';
-import styles, { ralph } from '../styles';
-
-const stackConfig = {
-  navigationOptions: {
-    header: {
-      style: {
-        backgroundColor: ralph.primaryColor,
-      },
-      tintColor: ralph.backgroundColor,
-    },
-  },
-};
-
-const tabConfig = {
-  tabBarOptions: {
-    activeTintColor: ralph.primaryColor,
-  },
-};
-
-const WelcomeNavigator = StackNavigator({
-  Welcome: {screen: Welcome},
-}, stackConfig);
-
-const CounterNavigator = StackNavigator({
-  Counter: {screen: Counter},
-}, stackConfig);
-
-const AppNavigator = TabNavigator({
-  Welcome: {screen: WelcomeNavigator},
-  Counter: {screen: CounterNavigator},
-}, tabConfig);
+import { AppNavigator } from '../routes';
+import styles from '../styles';
 
 const App = () => (
   <View style={styles.app}>
