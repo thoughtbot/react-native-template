7f4446f6ebe58ac0523c35caed34419071e916a2
Jake Craige <james.craige@gmail.com>
2017-02-10 12:44:17 -0800

    Add react-native-config
    Add the react-native-config package to support `.env` style configuration that
    supports debug and release builds and the variables are accessible from the
    native and app code.
    
    Adds a `.sample.env` file that gets copied to `.env.local` and `.env.production`
    when running `bin/setup`. These files are ignored since they contain potentially
    senstive API keys. The `.local` variant is used for debug builds while the
    `.production` is used for release builds.

--- TemplateProject/android/app/src/main/java/com/templateproject/MainApplication.java
+++ TemplateProject/android/app/src/main/java/com/templateproject/MainApplication.java
@@ -3,6 +3,7 @@ package com.templateproject;
 import android.app.Application;
 import android.util.Log;
 
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -24,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new ReactNativeConfigPackage()
       );
     }
   };
